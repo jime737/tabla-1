6919d9c13ecc67d5775830305b19e95cfdbbd1d0
jime737 <66139004+jime737@users.noreply.github.com>
2020-07-20 14:55:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> HOLA!! TABLA DE INTEGRANTES DEL PPI </h1>
+      <p>COMBATIENDO MIS BACTERIAS</p>
+
+    <div className="contendor2">
+      <table>
+<tr>
+      <th className="p1">Nombre</th>
+      <th className="p1">Apellidos</th>
+      <th className="p1">Correo</th>
+      <th className="p1">Celular</th>
+</tr>
+<tr>
+      <td> Jimena</td>
+      <td> Higuita Giraldo</td>
+      <td> Jimenahiguita246@gmail.com</td>
+      <td> 3024015487</td>
+ </tr>
+ <tr>
+      <td> Hellen</td>
+      <td>Tangarife Sucerquia</td>
+      <td> hellenzuce2004@gmail.com</td>
+      <td> 3113035652</td>
+ </tr>
+ <tr>
+      <td> Manuela</td>
+      <td> Vasquez Machado</td>
+      <td> manuelamaye123@gmail.com</td>
+      <td> 3014543339</td>
+ </tr>
+ <tr>
+      <td> Maria Fernanda</td>
+      <td> Correa Campero</td>
+      <td> Fernandacorrea887@gmail.com</td>
+      <td> 3006915315</td>
+ </tr>
+
+</table>
+</div>
+</div>
+
+  
+);
+}
